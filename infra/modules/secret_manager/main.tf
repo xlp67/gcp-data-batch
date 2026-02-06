2156728140de4318aebe4d81ede30eaaf908578e
xlp67 <thiiagowilliam@gmail.com>
2026-02-06 14:30:55 -0300

    feat(infra): infra/modules/secret_manager/main.tf Check OK

---
+++ infra/modules/secret_manager/main.tf
@@ -0,0 +1,11 @@
+resource "google_secret_manager_secret" "secret" {
+  project   = var.gcp_project_id
+  secret_id = var.secret_id
+  replication {
+    automatic = true
+  }
+}
+resource "google_secret_manager_secret_version" "secret_version" {
+  secret      = google_secret_manager_secret.secret.id
+  secret_data = var.secret_value
+}
